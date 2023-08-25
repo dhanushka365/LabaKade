f02a1ef1cca356b1f7147b832c03996effdc6ff9
uduwela96 <puduwel@mitrai.com>
2023-08-25 13:26:24 +0530

    work commited

--- LabaKade.Services.CouponAPI/Data/AppDbContext.cs
+++ LabaKade.Services.CouponAPI/Data/AppDbContext.cs
@@ -12,5 +12,28 @@ namespace LabaKade.Services.CouponAPI.Data
         }
 
         public DbSet<Coupon> Coupons { get; set;}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Coupon>().HasData(new Coupon
+            {
+                CouponId = 1,
+                CouponCode = "A001",
+                DiscountAmount =10,
+                MinAmount =20
+
+            });
+
+            modelBuilder.Entity<Coupon>().HasData(new Coupon
+            {
+                CouponId = 2,
+                CouponCode = "A002",
+                DiscountAmount = 20,
+                MinAmount = 30
+
+            });
+        }
     }
 }
